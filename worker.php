1d2590260c1aba772d3070804e8cd4fc15c5dc62
georgieff <lubo@georgieff.it>
2013-08-16 00:25:47 +0300

    little refactoring made * need script to split each of the cameras position and sort it by date + some code moving ourside the worker.php

--- worker.php
+++ worker.php
@@ -1,56 +1,92 @@
 <?php
 
-// https://www.facebook.com/feeds/page.php?id=388707501198026&format=json
-// is the URL that should return json data : )
-// if the worker is not working check the URL in your browser
-// if it's not working in the browser then we should research another way to fetch feed from facebook page : )
-// useful link may be this one: https://developers.facebook.com/tools/explorer/?method=GET&path=sdvr.stolichnapolicia%3Ffields%3Did%2Cname
-// play & enjoy! : )
-
-// ====
-// Set the string needed for the camera posts on the news feed
-$cameras_string = 'График на средствата за контрол на пътното движение';
-$page_feed_url_string = 'https://www.facebook.com/feeds/page.php?id=388707501198026&format=json';
-//some random user agent : D
-$curl_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1';
-
-//cURLing
-// Get cURL resource
-$curl = curl_init();
-// Set some options - we are passing in a useragent too here
-curl_setopt_array($curl, array(
-  CURLOPT_RETURNTRANSFER => 1,
-  CURLOPT_URL => $page_feed_url_string,
-  CURLOPT_USERAGENT => $curl_user_agent
-  ));
-// Send the request & save response to $resp
-$resp = curl_exec($curl);
-// Close request to clear up some resources
-curl_close($curl);
-
-
-$decoded_response = json_decode($resp);
-
+$worker = new SDVRWorker();
+if($worker->try_fetch_feed()) {
+  $decoded_response = $worker->get_cameras_results();
+} else {
+  $decoded_response = '{}';
+}
 echo "<meta charset='utf-8'>";
+echo "<pre>";
 
-//news count
-//echo count($decoded_response->entries). " news";
+var_dump($decoded_response);
 
-foreach ($decoded_response->entries as $key => $entry) {
-  if(strpos($entry->content, $cameras_string) !== false) {
-    $date_published = strtotime($entry->updated);
-    echo "<h3>" . "(" . date('H:m:s d/m/Y',$date_published) . ") - " . $entry->title . "</h3>";
-    echo "<br><div style='border:1px solid black; padding: 20px;'>";
-    echo "<b>ID:</b> sp" . $entry->id ."<br>";
-    echo "<b>CONTENT:</b> <br>" . $entry->content;
-    echo "</div>";
+class SDVRWorker
+{
+
+  // https://www.facebook.com/feeds/page.php?id=388707501198026&format=json
+  // is the URL that should return json data : )
+  // if the worker is not working check the URL in your browser
+  // if it's not working in the browser then we should research another way to fetch feed from facebook page : )
+  // useful link may be this one: https://developers.facebook.com/tools/explorer/?method=GET&path=sdvr.stolichnapolicia%3Ffields%3Did%2Cname
+  // play & enjoy! : )
+
+  // Set the string needed for the camera posts on the news feed
+  // property declaration
+  private $cameras_string = 'График на средствата за контрол на пътното движение';
+  private $page_feed_url_string = 'https://www.facebook.com/feeds/page.php?id=388707501198026&format=json';
+  //some random user agent : D
+  private $curl_user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/536.30.1 (KHTML, like Gecko) Version/6.0.5 Safari/536.30.1';
+
+  private $curl;
+  private $resp = '';
+
+  public function __construct() {
+    $this->curl = curl_init();
+  }
+
+  public function try_fetch_feed() {
+    try {
+      curl_setopt_array($this->curl, array(
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => $this->page_feed_url_string,
+        CURLOPT_USERAGENT => $this->curl_user_agent
+        ));
+      $this->resp = curl_exec($this->curl);
+      curl_close($this->curl);
+
+      return TRUE;
+    } catch (Exception $e) {
+
+      return FALSE;
+    }
+  }
+
+  public function get_all_result($in = 'plain') {
+    switch ($in) {
+      case 'object':
+      return json_decode($this->resp);
+      break;
+      case 'array':
+      return json_decode($this->resp, true);
+      break;
+      default:
+      return $this->resp;
+      break;
+    }
+  }
+
+  public function get_cameras_results() {
+    $all_results = $this->get_all_result('object');
+    $return_data = array();
+
+    foreach ($all_results->entries as $key => $entry) {
+
+      if(strpos($entry->content, $this->cameras_string) !== false) {
+        $day = null;
+        $day->date_published = strtotime($entry->updated);
+        $day->title = $entry->title;
+        $day->id = $entry->id;
+        $day->content = $entry->content;
+        $return_data[] = $day;
+      }
+    }
+
+    return $return_data;
   }
 }
 
-echo "<br><br><br><br><br><br><hr><br>";
-echo "<pre>";
 
-var_dump($decoded_response->entries);
 
 
 
